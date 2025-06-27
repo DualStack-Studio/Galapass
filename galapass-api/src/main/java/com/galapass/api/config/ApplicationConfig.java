254eac86ad56dac5244f5aa52194250ae195cb8e
Pablo <pasebarona@gmail.com>
2025-06-26 23:21:10 -0500

    OwnerDashboard Complete

--- galapass-api/src/main/java/com/galapass/api/config/ApplicationConfig.java
+++ galapass-api/src/main/java/com/galapass/api/config/ApplicationConfig.java
@@ -1,7 +1,6 @@
 package com.galapass.api.config;
 
 import com.galapass.api.repository.UserRepository;
-import com.galapass.api.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

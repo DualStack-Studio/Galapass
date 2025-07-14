f97d94aa7faa3a5cedfad34067213722218e8db7
DanMeraDev <dm375211@gmail.com>
2025-07-14 12:01:28 -0500

    docker

--- galapass-api/src/main/java/com/galapass/api/config/SecurityConfig.java
+++ galapass-api/src/main/java/com/galapass/api/config/SecurityConfig.java
@@ -57,7 +57,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOriginPattern("*");
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:5173",
+                "https://galapass.netlify.app"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
